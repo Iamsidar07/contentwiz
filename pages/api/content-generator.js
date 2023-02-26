85634a44c5a605d6f58527b33d9306c1cfaf44ca
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-02-26 21:41:04 +0530

    add log into api

--- pages/api/content-generator.js
+++ pages/api/content-generator.js
@@ -36,6 +36,7 @@ export default async function (req, res) {
         res.status(200).json({ result: completion.data.choices[0].text });
     } catch (error) {
         // Consider adjusting the error handling logic for your use case
+        console.log(error)
         if (error.response) {
             console.error(error.response.status, error.response.data);
             res.status(error.response.status).json(error.response.data);
