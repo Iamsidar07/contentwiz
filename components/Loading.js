51b22029ed3fee5d31eb018b2589a4f3dfe5a50b
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-02-26 12:35:51 +0530

    changed loading gif

--- components/Loading.js
+++ components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 const Loading = () => {
   return (
-    <Image src={"/loading-state.gif"} alt="loading" width={100} height={100}  />
+    <Image src={"/flickr-loading.gif"} alt="loading" width={100} height={100}  />
   )
 }
 
