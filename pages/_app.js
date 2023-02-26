6509a5c40b84ea8df7f5a6136adad9c116ec5e22
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-02-26 12:21:36 +0530

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,11 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <>
+    <Head>
+      <title>ContentWiz.com || Transform your writing with AI</title>
+      <meta name="description" content="Transform your writing with our AI-powered platform. Create high-quality, plagiarism-free content effortlessly for your blog, YouTube, Twitter, LinkedIn, and more. Join us and experience the power of AI writing today" />
+      <link rel="icon" href="/robot.png" />
+    </Head>
     <Navbar />
     <Component {...pageProps} />
     <Footer/>
