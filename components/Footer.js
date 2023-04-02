279b9234e16fe44d68749f5e45db96e5c972faee
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-04-02 16:27:03 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const Footer = () => {
   return (
-    <footer className='flex flex-1 py-8 items-center justify-center shadow bg-white '>
+    <footer className='flex flex-1 py-8 items-center justify-center shadow backdrop-blur-sm bg-gradient-to-b from-gray-900 via-purple-900 to-violet-600'>
       <div className='w-full flex flex-col md:flex-row items-center justify-center space-y-2 md:space-y-0'>
         <div className='flex items-center flex-col  md:flex-row space-y-2 md:space-x-2 md:space-y-0 px-5'>
           <Link href={"/privacy-policy"} className="underline">
