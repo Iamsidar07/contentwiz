2c74ef7100c645ef340f60e6070a7bb66dedd79b
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-04-02 19:03:44 +0530

    Update HomeFeature.js

--- components/HomeFeature.js
+++ components/HomeFeature.js
@@ -4,14 +4,14 @@ import Fade from 'react-reveal/Fade';
 const HomeFeature = ({ title, desc, image, alt, changeSequence }) => {
 
     return (
-        <section className={`flex flex-col-reverse  rounded-md md:flex-row justify-center bg-white ${changeSequence && "md:flex-row-reverse"} `}>
+        <section className={`flex flex-col-reverse  rounded-sm md:flex-row justify-center bg-white ${changeSequence && "md:flex-row-reverse"} `}>
             { changeSequence ? <Fade bottom>
-                    <div className='space-y-3 max-w-lg  p-2 bg-white rounded-md shadow-sm sm:shadow-none flex flex-col justify-center '>
+                    <div className='space-y-3 max-w-lg  p-2 bg-white rounded-sm shadow-sm sm:shadow-none flex flex-col justify-center '>
                         <h2 className='text-2xl md:text-3xl font-bold text-center md:text-left'>{title}</h2>
                         <p className='text-center md:text-left'>{desc}</p>
                     </div>
                 </Fade> : <Fade left>
-                        <div className='space-y-3 max-w-lg bg-white p-3 rounded-md shadow-sm sm:shadow-none flex flex-col justify-center'>
+                        <div className='space-y-3 max-w-lg bg-white p-3 rounded-sm shadow-sm sm:shadow-none flex flex-col justify-center'>
                         <h2 className='text-2xl md:text-3xl font-bold text-center md:text-left'>{title}</h2>
                         <p className='text-center md:text-left'>{desc}</p>
                     </div>
