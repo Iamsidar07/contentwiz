baee4497814621b55d5cce64bdbcf5a5f149d411
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-04-02 19:06:51 +0530

    Update HomeFeature.js

--- components/HomeFeature.js
+++ components/HomeFeature.js
@@ -6,12 +6,12 @@ const HomeFeature = ({ title, desc, image, alt, changeSequence }) => {
     return (
         <section className={`flex flex-col-reverse  rounded-sm md:flex-row justify-center bg-white ${changeSequence && "md:flex-row-reverse"} `}>
             { changeSequence ? <Fade bottom>
-                    <div className='space-y-3 max-w-lg  p-2 bg-white rounded-sm shadow-sm sm:shadow-none flex flex-col justify-center '>
+                    <div className='space-y-3 max-w-lg  p-2 bg-white rounded-sm shadow-md sm:shadow-none flex flex-col justify-center '>
                         <h2 className='text-2xl md:text-3xl font-bold text-center md:text-left'>{title}</h2>
                         <p className='text-center md:text-left'>{desc}</p>
                     </div>
                 </Fade> : <Fade left>
-                        <div className='space-y-3 max-w-lg bg-white p-3 rounded-sm shadow-sm sm:shadow-none flex flex-col justify-center'>
+                        <div className='space-y-3 max-w-lg bg-white p-3 rounded-sm shadow-md sm:shadow-none flex flex-col justify-center'>
                         <h2 className='text-2xl md:text-3xl font-bold text-center md:text-left'>{title}</h2>
                         <p className='text-center md:text-left'>{desc}</p>
                     </div>
