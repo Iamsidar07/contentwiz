d54be416c42dd116f7d5f5e1e12c552406121373
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-04-02 16:49:41 +0530

    Update HomeFeature.js

--- components/HomeFeature.js
+++ components/HomeFeature.js
@@ -4,7 +4,7 @@ import Fade from 'react-reveal/Fade';
 const HomeFeature = ({ title, desc, image, alt, changeSequence }) => {
 
     return (
-        <section className={`flex flex-col-reverse md:flex-row justify-between bg-transparent  ${changeSequence && "md:flex-row-reverse bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900"} }>
+        <section className={`flex flex-col-reverse md:flex-row justify-between bg-transparent ${changeSequence && "md:flex-row-reverse bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900"} }>
             {
                 changeSequence ? <Fade bottom>
                     <div className='space-y-3 max-w-lg bg-white p-3 bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r bg-opacity-0.8 backdrop-blur-sm rounded-md shadow-sm sm:shadow-none flex flex-col justify-center '>
