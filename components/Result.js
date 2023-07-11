47cc26dce46469ec3241ee884cf3dce2b8809a22
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-07-11 14:44:53 +0530

    make result editable

--- components/Result.js
+++ components/Result.js
@@ -7,7 +7,7 @@ const Result = ({ result }) => {
     <Fade bottom>
       <div className={"w-full  md:max-w-7xl mx-auto bg-white p-2 md:p-4 mb-6 relative rounded-2xl shadow"}>
         <FiCopy color='black' size={18} className="absolute top-2 right-2 cursor-pointer" onClick={(e) => copyToClipboard(result)} />
-        <div className='break-words leading-2' dangerouslySetInnerHTML={{ __html: result.replaceAll('\n', '<br/>') }} />
+        <div className='break-words leading-2' contentEditable={true} dangerouslySetInnerHTML={{ __html: result.replaceAll('\n', '<br/>') }} />
       </div>
     </Fade>
   )
